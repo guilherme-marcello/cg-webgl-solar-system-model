1b49cf054dafe27cb98abb7b7beb471846a946eb
guilherme-marcello <guilemosm@gmail.com>
2023-12-01 16:13:55 +0000

    Added timescale to model

--- libs/celestial.js/celestial-full.js
+++ libs/celestial.js/celestial-full.js
@@ -21,12 +21,18 @@ class CelestialBody {
     }
 }
 
-function createSun(gl) {
+function createSun(gl, timescale) {
     // texture src: https://svs.gsfc.nasa.gov/30362
     let name = "Sun";
+    let dayPeriod = 27;
+
     let buffer = twgl.primitives.createSphereBufferInfo(gl, 2.5, 20, 20);
     let texture = twgl.createTexture(gl, { src: 'textures/sun.jpeg' });
-    let rotationF = (time) => m4.rotationY(1*time);
+
+    let rotationPeriod = timescale * dayPeriod;
+    let rotationAngularSpeed = 2*Math.PI / rotationPeriod;
+    let rotationF = (time) => m4.rotationY(rotationAngularSpeed * time);
+
     let translationF = (time) => m4.translation([0, 0, 0]);
 
     return new CelestialBody(
@@ -38,13 +44,31 @@ function createSun(gl) {
     );
 }
 
-function createMercury(gl) {
+function createMercury(gl, timescale) {
     // texture src: https://svs.gsfc.nasa.gov/11197
     let name =  "Mercury";
+    let yearPeriod = 88; 
+    let dayPeriod = 176;
+
     let buffer = twgl.primitives.createSphereBufferInfo(gl, 0.1, 20, 20);
     let texture = twgl.createTexture(gl, { src: 'textures/mercury.jpeg' });
-    let rotationF = (time) => m4.rotationY(10*time);
-    let translationF = (time) => m4.translation([5*Math.sin(2*time), 0, 5*Math.cos(2*time)]);
+
+
+    let rotationPeriod = timescale * dayPeriod;
+    let rotationAngularSpeed = 2*Math.PI / rotationPeriod;
+    let rotationF = (time) => m4.rotationY(rotationAngularSpeed * time);
+
+    let translationPeriod = timescale * yearPeriod;
+    let translationAngularSpeed = 2*Math.PI / translationPeriod;
+    let translationF = (time) => {
+        const a = 5; // semi-major axis
+        const b = 5; // semi-minor axis
+
+        const x = a * Math.cos(translationAngularSpeed * time);
+        const y = -b * Math.sin(translationAngularSpeed * time);
+    
+        return twgl.m4.translation([x, 0, y]);
+    };
 
     return new CelestialBody(
         name,
@@ -55,13 +79,30 @@ function createMercury(gl) {
     );
 }
 
-function createEarth(gl) {
+function createEarth(gl, timescale) {
     // texture src: https://visibleearth.nasa.gov/images/73580/january-blue-marble-next-generation-w-topography-and-bathymetry
     let name =  "Earth";
+    let yearPeriod = 365; 
+    let dayPeriod = 1;
+
     let buffer = twgl.primitives.createSphereBufferInfo(gl, 0.5, 20, 20);
     let texture = twgl.createTexture(gl, { src: 'textures/earth.jpg' });
-    let rotationF = (time) => m4.rotationY(50*time);
-    let translationF = (time) => m4.translation([10*Math.sin(time), 0, 10*Math.cos(time)]);
+
+    let rotationPeriod = timescale * dayPeriod;
+    let rotationAngularSpeed = 2*Math.PI / rotationPeriod;
+    let rotationF = (time) => m4.rotationY(rotationAngularSpeed * time);
+
+    let translationPeriod = timescale * yearPeriod;
+    let translationAngularSpeed = 2*Math.PI / translationPeriod;
+    let translationF = (time) => {
+        const a = 10; // semi-major axis
+        const b = 10; // semi-minor axis
+
+        const x = a * Math.cos(translationAngularSpeed * time);
+        const y = -b * Math.sin(translationAngularSpeed * time);
+    
+        return twgl.m4.translation([x, 0, y]);
+    };
 
     return new CelestialBody(
         name,
@@ -72,13 +113,30 @@ function createEarth(gl) {
     );
 }
 
-function createMars(gl) {
+function createMars(gl, timescale) {
     // texture src: https://nasa3d.arc.nasa.gov/detail/mar0kuu2
     let name =  "Mars";
+    let yearPeriod = 687; 
+    let dayPeriod = 1.025;
+
     let buffer = twgl.primitives.createSphereBufferInfo(gl, 0.6, 20, 20);
     let texture = twgl.createTexture(gl, { src: 'textures/mars.jpeg' });
-    let rotationF = (time) => m4.rotationY(25*time);
-    let translationF = (time) => m4.translation([15*Math.sin(0.7*time), 0, 15*Math.cos(0.7*time)]);
+
+    let rotationPeriod = timescale * dayPeriod;
+    let rotationAngularSpeed = 2*Math.PI / rotationPeriod;
+    let rotationF = (time) => m4.rotationY(rotationAngularSpeed * time);
+
+    let translationPeriod = timescale * yearPeriod;
+    let translationAngularSpeed = 2*Math.PI / translationPeriod;
+    let translationF = (time) => {
+        const a = 16; // semi-major axis
+        const b = 15; // semi-minor axis
+
+        const x = a * Math.cos(translationAngularSpeed * time);
+        const y = -b * Math.sin(translationAngularSpeed * time);
+    
+        return twgl.m4.translation([x, 0, y]);
+    };
 
     return new CelestialBody(
         name,
@@ -89,13 +147,30 @@ function createMars(gl) {
     );
 }
 
-function createJupiter(gl) {
+function createJupiter(gl, timescale) {
     // texture src: https://svs.gsfc.nasa.gov/12021
     let name =  "Jupiter";
+    let yearPeriod = 4333; 
+    let dayPeriod = 0.4167;
+
     let buffer = twgl.primitives.createSphereBufferInfo(gl, 1, 20, 20);
     let texture = twgl.createTexture(gl, { src: 'textures/jupiter.jpeg' });
-    let rotationF = (time) => m4.rotationY(10*time);
-    let translationF = (time) => (time) => m4.translation([20*Math.sin(0.5*time), 0, 20*Math.cos(0.5*time)]);
+
+    let rotationPeriod = timescale * dayPeriod;
+    let rotationAngularSpeed = 2*Math.PI / rotationPeriod;
+    let rotationF = (time) => m4.rotationY(rotationAngularSpeed * time);
+
+    let translationPeriod = timescale * yearPeriod;
+    let translationAngularSpeed = 2*Math.PI / translationPeriod;
+    let translationF = (time) => {
+        const a = 21; // semi-major axis
+        const b = 20; // semi-minor axis
+
+        const x = a * Math.cos(translationAngularSpeed * time);
+        const y = -b * Math.sin(translationAngularSpeed * time);
+    
+        return twgl.m4.translation([x, 0, y]);
+    };
 
     return new CelestialBody(
         name,
@@ -106,23 +181,23 @@ function createJupiter(gl) {
     );
 }
 
-function create(gl, name) {
+function create(gl, timescale, name) {
     switch (name.toLowerCase()) {
         case "sun":
             console.log("Creating Sun!");
-            return createSun(gl);
+            return createSun(gl, timescale);
         case "mercury":
             console.log("Creating Mercury!");
-            return createMercury(gl);
+            return createMercury(gl, timescale);
         case "earth":
             console.log("Creating Earth!");
-            return createEarth(gl);
+            return createEarth(gl, timescale);
         case "mars":
             console.log("Creating Mars!");
-            return createMars(gl);
+            return createMars(gl, timescale);
         case "jupiter":
             console.log("Creating Jupiter!");
-            return createJupiter(gl);
+            return createJupiter(gl, timescale);
         default:
             console.log("Unable to create " + name);
             return null;
